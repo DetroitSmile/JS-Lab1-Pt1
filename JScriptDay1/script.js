c687f50f3ce5abc982d1cdd0d4b7d10431ccfc86
DetroitSmile <45802904+DetroitSmile@users.noreply.github.com>
2019-01-23 11:07:38 -0500

    Add files via upload

--- JScriptDay1/script.js
+++ JScriptDay1/script.js
@@ -1,18 +1,18 @@
 "use strict";
 
-let wins = 0;
-let grantHp = 10;
-let userHp = 40;
-let intro = prompt("Dare to challenge the Almighty Grant?")
+    let intro = prompt("Dare to challenge the Almighty Grant?");
 
-{
-if (intro === "yes" || "Yes") {
+if (intro === "yes" || intro === "Yes") {
     let userName = prompt("Whats your name, human?");
+    let wins = 0;
+    let grantHp = 10;
+    let userHp = 40;
+
     console.log(`Let's get it crackin! ${userName}'s starting health is ${userHp} and the Almighty Grant's health is ${grantHp}!`);
     while (grantHp > 0) {
         console.log(`${userName} health: ${userHp -= Math.floor((Math.random()* 2) + 1)}`);
         console.log(`Almighty Grant's health: ${grantHp -= Math.floor((Math.random()* 2) +1)}`);
-        if (wins >= 3 || grantHp <= 0) {
+        if (wins >= 3 && grantHp <= 0) {
             console.log(`"Winner winner ${userName}! You've defeated the Almighty Grant!"`);
             break;
         } else if (userHp <= 0) {
@@ -24,7 +24,7 @@ if (intro === "yes" || "Yes") {
             grantHp = 10;
         }
     }
-} else if (intro === "no" || "No") {
+} else {
     console.log("Later, hater!");
     }
-}
+
